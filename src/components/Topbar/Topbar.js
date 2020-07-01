f97db462eaf94cd7233f0f87802a9373145e9ee4
developer-haku <51524534+developer-haku@users.noreply.github.com>
2020-07-01 18:10:30 -0400

    add setting menu and reset alert triggler to buttons

--- src/components/Topbar/Topbar.js
+++ src/components/Topbar/Topbar.js
@@ -1,13 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import { AppBar, Toolbar, IconButton, Typography } from "@material-ui/core";
 import { Menu, Settings, GitHub, Cached } from "@material-ui/icons";
 
 import styles from "./Topbar.module.css";
 import logo from "../../assets/images/logo.png";
+import ResetAlert from "../Reset/Reset";
+import SettingMenu from "../Setting/Setting";
 
-const topbar = (props) => {
+const Topbar = (props) => {
+  const [openResetAlert, setOpenResetAlert] = useState(false);
+  const [openMenu, setOpenMenu] = useState(false);
+
+  const resetAlert = () => {
+    setOpenResetAlert(true);
+  };
+
+  const closeAlert = () => {
+    setOpenResetAlert(false);
+  };
+
+  const settingMenu = () => {
+    setOpenMenu(true);
+  };
+
+  const closeMenu = () => {
+    setOpenMenu(false);
+  };
+
   const forwardGithub = () => {
-    window.open("https://github.com/developer-haku/bangumi", "_black");
+    window.open("https://github.com/developer-haku/bangumi");
   };
 
   return (
@@ -28,14 +49,6 @@ const topbar = (props) => {
             className={styles.rightItem}
             edge="end"
             color="inherit"
-            aria-label="cached"
-          >
-            <Cached />
-          </IconButton>
-          <IconButton
-            className={styles.rightItem}
-            edge="end"
-            color="inherit"
             aria-label="github"
             onClick={forwardGithub}
           >
@@ -44,15 +57,27 @@ const topbar = (props) => {
           <IconButton
             className={styles.rightItem}
             edge="end"
+            onClick={resetAlert}
+            color="inherit"
+            aria-label="cached"
+          >
+            <Cached />
+          </IconButton>
+          <IconButton
+            className={styles.rightItem}
+            onClick={settingMenu}
+            edge="end"
             color="inherit"
             aria-label="menu"
           >
             <Settings />
           </IconButton>
+          {openResetAlert && <ResetAlert open={openResetAlert} close={closeAlert} />}
+          {openMenu && <SettingMenu open={openMenu} close={closeMenu} />}
         </div>
       </Toolbar>
     </AppBar>
   );
 };
 
-export default topbar;
+export default Topbar;
