68f545aaedfb6e1e5eb388c54ddbf357f142aa45
developer-haku <51524534+developer-haku@users.noreply.github.com>
2020-06-30 21:52:29 -0400

    remove unused codes

--- src/components/FullAnimeInfo/InfoSection/TitleSection/TitleSection.js
+++ src/components/FullAnimeInfo/InfoSection/TitleSection/TitleSection.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { Link, Typography } from "@material-ui/core";
 import { AccessTime, CalendarToday } from "@material-ui/icons";
 import { Rating } from "@material-ui/lab";
@@ -18,7 +18,6 @@ const TitleSection = (props) => {
   const weekday = translateWeekday(props.weekday);
   const siteMeta = JSON.parse(localStorage.getItem("bd_site_meta"));
   const data = getLocalBangumiDataById("" + props.id);
-  const regex = /[\u3002|\uff1f|\uff01|\uff0c|\u3001|\uff1b|\uff1a|\u201c|\u201d|\u2018|\u2019|\uff08|\uff09|\u300a|\u300b|\u3008|\u3009|\u3010|\u3011|\u300e|\u300f|\u300c|\u300d|\ufe43|\ufe44|\u3014|\u3015|\u2026|\u2014|\uff5e|\ufe4f|\uffe5]/g;
   let links = null;
 
   const imgOnLoad = () => {
@@ -81,8 +80,6 @@ const TitleSection = (props) => {
   const siteHandler = (site, id) => {
     window.open(siteMeta[site].urlTemplate.replace("{{id}}", id));
   };
-
-  console.log(props.titleCN);
 
   return (
     <div className={styles.titleSection}>
