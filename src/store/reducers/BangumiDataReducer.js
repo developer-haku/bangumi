83da6f7d63c2f67836f5e38e3e3d5109b078df61
developer-haku <51524534+developer-haku@users.noreply.github.com>
2020-07-02 15:00:43 -0400

    remove deprecated state and functions, add loading state and initialization

--- src/store/reducers/BangumiDataReducer.js
+++ src/store/reducers/BangumiDataReducer.js
@@ -1,44 +1,20 @@
 import * as actionType from "../actions/ActionTypes";
 
 const defaultState = {
-  siteMeta: {},
-  fullAnimeList: [],
-  currentSelectedAnimeList: [],
-  years: [],
-  latest: true,
-  updateToLatest: true,
+  initialized: false
 };
 
-const fetchBangumiData = (state, action) => {
+const initialization = (state, action) => {
   return {
     ...state,
-    siteMeta: action.siteMeta,
-    fullAnimeList: action.items,
-  };
-};
-
-const selectAnimeBySeason = (state, action) => {
-  return {
-    ...state,
-    currentSelectedAnimeList: action.list,
-  };
-};
-
-const initializeYearList = (state, action) => {
-  return {
-    ...state,
-    years: action.years
+    initialized: action.initialized
   }
 }
 
 const reducer = (state = defaultState, action) => {
   switch (action.type) {
-    case actionType.FETCH_BANGUMI_DATA:
-      return fetchBangumiData(state, action);
-    case actionType.SELECT_ANIME_BY_SEASON:
-      return selectAnimeBySeason(state, action);
-    case actionType.INITIALIZE_YEAR_LIST:
-      return initializeYearList(state, action);
+    case actionType.INITIALIZATION:
+      return initialization(state, action);
     default:
       return state;
   }
