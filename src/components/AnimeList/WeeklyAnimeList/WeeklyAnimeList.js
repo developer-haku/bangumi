ad26f08d34df131421af09a8e258b017f4e10ed8
developer-haku <51524534+developer-haku@users.noreply.github.com>
2020-07-06 18:04:42 -0400

    outsourcing data to a other component, changing the weekly page general style

--- src/components/AnimeList/WeeklyAnimeList/WeeklyAnimeList.js
+++ src/components/AnimeList/WeeklyAnimeList/WeeklyAnimeList.js
@@ -1,9 +1,6 @@
 import React from "react";
-import { Divider, Typography } from "@material-ui/core";
-import { CalendarToday } from "@material-ui/icons";
 
-import styles from "./WeeklyAnimeList.module.css";
-import AnimeCard from "../../AnimeCard/AnimeCard";
+import AnimeColumnList from "../../AnimeColumnList/AnimeColumnList";
 import { getSeasonRange } from "../../../utils/utils";
 
 const WeeklyAnimeList = (props) => {
@@ -24,47 +21,21 @@ const WeeklyAnimeList = (props) => {
 
   for (let day = 0; day < 7; day++) {
     list.push(
-      selectedItems
-        .filter((i) => {
-          return new Date(i.begin).getDay() === day;
-        })
-        .map((item) => {
-          const key = item.sites.find((i) => i.site === "bangumi").id;
-          return <AnimeCard key={key} anime={item} bangumiId={key} />;
-        })
+      selectedItems.filter((i) => {
+        return new Date(i.begin).getDay() === day;
+      })
     );
   }
-
-  const weekdayCN = [
-    "星期日",
-    "星期一",
-    "星期二",
-    "星期三",
-    "星期四",
-    "星期五",
-    "星期六",
-  ];
 
   return (
     <div>
       {list.map((l) => {
         return (
-          <div key={list.indexOf(l)}>
-            <Typography
-              className={
-                props.openSidebar
-                  ? styles.animeList
-                  : styles.animeListFullScreen
-              }
-              variant="h5"
-              gutterBottom
-            >
-              <CalendarToday className={styles.calendarIcon} />
-              {weekdayCN[list.indexOf(l)]}
-            </Typography>
-            <Divider />
-            <div>{l}</div>
-          </div>
+          <AnimeColumnList
+            key={list.indexOf(l)}
+            list={l}
+            weekday={list.indexOf(l)}
+          />
         );
       })}
     </div>
