260cc2c9a6b00e5a65a5c9cfcea4f244cf171d00
developer-haku <51524534+developer-haku@users.noreply.github.com>
2020-07-06 21:10:33 -0400

    Updates

---
+++ precache-manifest.b3ee08e8c6433bed5e2e66ef6455ab1f.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6936a9af4712f89e70cd1ba733d0ec25",
+    "url": "/bangumi/index.html"
+  },
+  {
+    "revision": "d08951d13200440ef0bc",
+    "url": "/bangumi/static/css/2.18fb2ca0.chunk.css"
+  },
+  {
+    "revision": "49b38f42c78eb951d3ca",
+    "url": "/bangumi/static/css/main.0a314692.chunk.css"
+  },
+  {
+    "revision": "d08951d13200440ef0bc",
+    "url": "/bangumi/static/js/2.da299d83.chunk.js"
+  },
+  {
+    "revision": "b360ca8930053283129a923ad66ccd36",
+    "url": "/bangumi/static/js/2.da299d83.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "49b38f42c78eb951d3ca",
+    "url": "/bangumi/static/js/main.d795ed03.chunk.js"
+  },
+  {
+    "revision": "d2cd880d415984cf6975",
+    "url": "/bangumi/static/js/runtime-main.aea1f8fd.js"
+  },
+  {
+    "revision": "cfa5cba721a2cb0b0976ececdbcf045a",
+    "url": "/bangumi/static/media/CharacterPlaceholder.cfa5cba7.jpg"
+  },
+  {
+    "revision": "1d5b9e47d7226322cea1cce8d4f91c79",
+    "url": "/bangumi/static/media/autumn.1d5b9e47.svg"
+  },
+  {
+    "revision": "92565b85517eacba6bcd75a8dc1fa4a2",
+    "url": "/bangumi/static/media/friday.92565b85.svg"
+  },
+  {
+    "revision": "b22a0a71d2f6bd524bea8c916cf7509d",
+    "url": "/bangumi/static/media/hkgyokk.b22a0a71.ttf"
+  },
+  {
+    "revision": "d3377a06615902ed3c93a3bb3da5336a",
+    "url": "/bangumi/static/media/loading.d3377a06.svg"
+  },
+  {
+    "revision": "e676953ad17c535cbcb16f64aee64ffe",
+    "url": "/bangumi/static/media/logo.e676953a.png"
+  },
+  {
+    "revision": "2022dabb16132b6ae313ed34f9104b0c",
+    "url": "/bangumi/static/media/monday.2022dabb.svg"
+  },
+  {
+    "revision": "44285a861af3646f8bbb6bf4baadbd37",
+    "url": "/bangumi/static/media/myne_cry.44285a86.png"
+  },
+  {
+    "revision": "0e0ea202b50170e1ed0b2f53b937a798",
+    "url": "/bangumi/static/media/myne_sit.0e0ea202.png"
+  },
+  {
+    "revision": "6ce0da6269157f414087add84d401022",
+    "url": "/bangumi/static/media/nocover.6ce0da62.jpg"
+  },
+  {
+    "revision": "1bd306965e8946c27e31f42442c7d0ad",
+    "url": "/bangumi/static/media/not_air.1bd30696.jpg"
+  },
+  {
+    "revision": "4bb326a58ea99b2ee9131eb9f7270812",
+    "url": "/bangumi/static/media/saturday.4bb326a5.svg"
+  },
+  {
+    "revision": "fb5137d258f1739700a165df3047e126",
+    "url": "/bangumi/static/media/season_empty.fb5137d2.jpg"
+  },
+  {
+    "revision": "7be2886519834cf3897b108ebce4257c",
+    "url": "/bangumi/static/media/spring.7be28865.svg"
+  },
+  {
+    "revision": "6738f47de08d97c803768f15a95e277b",
+    "url": "/bangumi/static/media/summer.6738f47d.svg"
+  },
+  {
+    "revision": "471b7676d4835b7056800d5242748608",
+    "url": "/bangumi/static/media/sunday.471b7676.svg"
+  },
+  {
+    "revision": "ebe26385597e9a2494e2d4f2ccc6de2f",
+    "url": "/bangumi/static/media/thursday.ebe26385.svg"
+  },
+  {
+    "revision": "ff792e5956cbae705fb583b38b491e53",
+    "url": "/bangumi/static/media/tuesday.ff792e59.svg"
+  },
+  {
+    "revision": "c5e355ed7a35cbcec09e0d425dc93da6",
+    "url": "/bangumi/static/media/wednesday.c5e355ed.svg"
+  },
+  {
+    "revision": "50821822c4c13634ee7e094e18330fb9",
+    "url": "/bangumi/static/media/winter.50821822.svg"
+  }
+]);
