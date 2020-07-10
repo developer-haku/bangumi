4b8300a004f48f8ba8c858de7624abedf17adf7b
developer-haku <51524534+developer-haku@users.noreply.github.com>
2020-07-09 22:06:57 -0400

    convert image url to https

--- src/service/api/BangumiApi.js
+++ src/service/api/BangumiApi.js
@@ -1,5 +1,7 @@
 import axios from "axios";
 
+import { forceHttps } from "../../utils/utils";
+
 const fetchBangumiApiData = async (id, size, corsUrl) => {
   const response = await axios
     .get(`${corsUrl}https://api.bgm.tv/subject/${id}?responseGroup=${size}`)
@@ -12,7 +14,7 @@ const reassembleBasicData = (data) => {
   let reassembledData = {};
   reassembledData.name = data ? data.name : null;
   reassembledData.nameCn = data ? (data["name_cn"] !== "" ? data["name_cn"] : null) : null;
-  reassembledData.image = data ? (data.images ? data.images.large : "") : "";
+  reassembledData.image = data ? (data.images ? forceHttps(data.images.large) : "") : "";
   reassembledData.score = data ? (data.rating ? data.rating.score : 0) : 0;
   return reassembledData;
 };
