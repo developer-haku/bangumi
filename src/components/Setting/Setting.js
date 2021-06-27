f408ed27ae34acdd42eff1841203eeae2d17c35d
developer-haku <51524534+developer-haku@users.noreply.github.com>
2021-06-27 18:16:38 -0400

    fixed: remove multiple APIs

--- src/components/Setting/Setting.js
+++ src/components/Setting/Setting.js
@@ -10,7 +10,6 @@ import {
   RadioGroup,
   FormControlLabel,
   Radio,
-  TextField,
   Divider,
 } from "@material-ui/core";
 
@@ -20,57 +19,39 @@ const Setting = (props) => {
   const [cardApi, setCardApi] = useState(
     JSON.parse(localStorage.getItem("setting")).cardApi
   );
-  const [pageApi, setPageApi] = useState(
+  const [pageApi] = useState(
     JSON.parse(localStorage.getItem("setting")).pageApi
   );
-  const [cors, setCors] = useState(
+  const [cors] = useState(
     JSON.parse(localStorage.getItem("setting")).cors.proxy
   );
-  const [corsUrl, setCorsUrl] = useState(
+  const [corsUrl] = useState(
     JSON.parse(localStorage.getItem("setting")).cors.url
   );
-  const [helperText, setHelperText] = useState("");
 
   const apply = () => {
-    if (cors === "custom" && corsUrl === "") {
-      setHelperText("URL不能是空白");
-      return;
-    } else {
-      localStorage.setItem(
-        "setting",
-        JSON.stringify({
-          cardApi: cardApi,
-          pageApi: pageApi,
-          cors: {
-            proxy: cors,
-            url:
-              cors === "custom"
-                ? corsUrl.slice(-1) !== "/"
-                  ? corsUrl + "/"
-                  : corsUrl
-                : "https://cors-anywhere.herokuapp.com/",
-          },
-        })
-      );
-      props.close();
-      window.location.reload();
-    }
+    localStorage.setItem(
+      "setting",
+      JSON.stringify({
+        cardApi: cardApi,
+        pageApi: pageApi,
+        cors: {
+          proxy: cors,
+          url:
+            cors === "custom"
+              ? corsUrl.slice(-1) !== "/"
+                ? corsUrl + "/"
+                : corsUrl
+              : "https://cors-anywhere.herokuapp.com/",
+        },
+      })
+    );
+    props.close();
+    window.location.reload();
   };
 
   const cardApiChangeHandler = (event) => {
     setCardApi(event.target.value);
-  };
-  const pageApiChangeHandler = (event) => {
-    setPageApi(event.target.value);
-  };
-
-  const corsChangeHandler = (event) => {
-    setCors(event.target.value);
-  };
-
-  const corsUrlChangeHandler = (event) => {
-    setCorsUrl(event.target.value);
-    if (event.target.value !== "") setHelperText("");
   };
 
   return (
@@ -96,61 +77,8 @@ const Setting = (props) => {
               value="bangumiSubject"
               label="Bangumi-Subject"
             />
-            <FormControlLabel
-              control={<Radio color="primary" />}
-              value="bangumiCORS"
-              label="Bangumi API(CORS)"
-            />
-            <FormControlLabel
-              control={<Radio color="primary" />}
-              value="netabare"
-              label="netaba.re"
-            />
           </RadioGroup>
           <Divider className={styles.divider} />
-          <FormLabel component="legend">详细页API源</FormLabel>
-          <RadioGroup value={pageApi} onChange={pageApiChangeHandler}>
-            <FormControlLabel
-              control={<Radio color="primary" />}
-              value="default"
-              label="Bangumi-Subject + Bangumi-GA(默认)"
-            />
-            <FormControlLabel
-              control={<Radio color="primary" />}
-              value="bangumiApi"
-              label="Bangumi-Subject + Bangumi API(CORS)"
-            />
-            <FormControlLabel
-              control={<Radio color="primary" />}
-              value="netabare"
-              label="Bangumi-Subject + Netaba.re"
-            />
-          </RadioGroup>
-          <Divider className={styles.divider} />
-          <FormLabel component="legend">CORS代理选择</FormLabel>
-          <RadioGroup value={cors} onChange={corsChangeHandler}>
-            <FormControlLabel
-              control={<Radio color="primary" />}
-              value="heroku"
-              label="cors-anywhere heroku服务器(海外)"
-            />
-            <FormControlLabel
-              control={<Radio color="primary" />}
-              value="custom"
-              label={
-                <TextField
-                  id="cors-custom-url"
-                  label="自定义"
-                  defaultValue={cors === "custom" ? corsUrl : ""}
-                  disabled={cors === "custom" ? false : true}
-                  margin="dense"
-                  variant="outlined"
-                  helperText={helperText}
-                  onChange={corsUrlChangeHandler}
-                />
-              }
-            />
-          </RadioGroup>
         </FormControl>
       </DialogContent>
       <DialogActions>
